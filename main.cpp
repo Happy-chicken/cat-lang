ea339549fd4249773ca08b591814af0cefa21edb
Bu Yi <Old_BuYi@outlook.com>
2026-01-30 15:37:02 +0100

    update command line using llvm::cl

--- main.cpp
+++ main.cpp
@@ -1,42 +1,57 @@
 // #include "ASTPrinter.hpp"
 #include "Cat.hpp"
-#include <iostream>
-
+#include <llvm-c-20/llvm-c/Types.h>
+#include <llvm/IR/DiagnosticInfo.h>
+#include <llvm/IR/Value.h>
+#include <llvm/Support/CommandLine.h>
 int main(int argc, char *argv[]) {
-    std::string filePath;// = "/home/buyi/code/cat-lang/test/test.cat";
-    std::string mode;
     Cat cat(argc, argv);
+    enum OptLv { O0,
+                 O1,
+                 O2,
+                 O3 };
+    llvm::cl::opt<string> mode(
+        llvm::cl::Positional,
+        llvm::cl::desc("<mode>"),
+        llvm::cl::Required
+    );
+    llvm::cl::opt<string> inputFile(llvm::cl::Positional, llvm::cl::desc("<input file>"), llvm::cl::Required);
+    llvm::cl::opt<string> outputFile("o", llvm::cl::desc("Specify output file name"), llvm::cl::value_desc("filename"));
+    llvm::cl::opt<OptLv> optLv(
+        "Level",
+        llvm::cl::desc("Choose optimization level"),
+        llvm::cl::Prefix,
+        llvm::cl::values(
+            clEnumValN(OptLv::O0, "0", "No optimization"),
+            clEnumValN(OptLv::O1, "1", "Trival optimization"),
+            clEnumValN(OptLv::O2, "2", "Default optimization"),
+            clEnumValN(OptLv::O3, "3", "Expensive optimization")
+        ),
+        llvm::cl::init(OptLv::O0)
+    );
+    llvm::cl::ParseCommandLineOptions(argc, argv, "Cat Language Compiler!\n");
+    // = "/home/buyi/code/cat-lang/test/test.cat";
     cat.isUseJIT = true;
     llvm::OptimizationLevel optLevel = llvm::OptimizationLevel::O0;
-    if (argc == 4) {
-        mode = argv[1];
-        filePath = argv[3];
-        if (std::string(argv[2]) == "-O0") {
+    switch (optLv) {
+        case OptLv::O0:
             optLevel = llvm::OptimizationLevel::O0;
-        } else if (std::string(argv[2]) == "-O1") {
+            break;
+        case OptLv::O1:
             optLevel = llvm::OptimizationLevel::O1;
-        } else if (std::string(argv[2]) == "-O2") {
+            break;
+        case OptLv::O2:
             optLevel = llvm::OptimizationLevel::O2;
-        } else if (std::string(argv[2]) == "-O3") {
+            break;
+        case OptLv::O3:
             optLevel = llvm::OptimizationLevel::O3;
-        } else {
-            std::cout << "Unknown optimization level: " << argv[2] << std::endl;
-            return 1;
-        }
-        // if (mode == "run") cat.runFile(filePath);
-        if (mode == "build") cat.buildFile(filePath, optLevel);
-    } else if (argc == 3) {
-        mode = argv[1];
-        filePath = argv[2];
-        if (mode == "run") {
-            // cat.runFile(filePath);
-        } else if (mode == "build") {
-            cat.buildFile(filePath, llvm::OptimizationLevel::O0);
-        }
-    } else {
-        std::cout << "\t[usage]: cat build <opt_level> <file.cat>\n\t\t cat run <file.cat>" << std::endl;
-        std::cout << cat.logo << std::endl;
+            break;
     }
-
+    if (mode == "run") {
+        // cat.runFile(filePath);
+    } else if (mode == "build") {
+        cat.buildFile(inputFile, optLevel);
+    }
     return 0;
 }
+
